b3604c530edc2935aaefd7e4c373f18f19cce51c
Lawrence Ephrim <ephrimlawrence0@gmail.com>
2025-07-11 15:15:26 +0000

    fix: update site title

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = 'Lawrence Ephrim';
+export const SITE_DESCRIPTION = 'Welcome to my personal portfolio website, showcasing my projects, skills, and experience as a developer. Explore my work, blog posts, and more.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
