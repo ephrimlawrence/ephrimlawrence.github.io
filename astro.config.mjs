7d047f130b6c115d6c5ab45e6c92af46124bab92
Lawrence Ephrim <ephrimlawrence0@gmail.com>
2025-07-16 22:14:04 +0000

    fix: trigger build

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://ephrimlawrence.github.io",
-	base: "ephrimlawrence",
+	base: "/",
 	integrations: [mdx(), sitemap(), tailwind()],
 });
