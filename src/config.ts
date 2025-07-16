373c5b640ce82036b8e537353a8cf8207b611510
Lawrence Ephrim <ephrimlawrence0@gmail.com>
2025-07-16 21:41:47 +0000

    Redesign site with astro template (#1)
    * refactor: delete old template
    
    * feat: add new template
    
    * fix: update site title
    
    * fix: update sidebar footer
    
    * feat: update cv
    
    * feat: update cv
    
    * feat: update cv
    
    * feat: update cv
    
    * refactor: remove store section
    
    * update profile
    
    * update profile
    
    * update profile
    
    * update footer
    
    * update favicon
    
    * feat: add cv pdf
    
    * fix: update projects
    
    * update index page
    
    * update profile image
    
    * ci: update for github deploy

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Lawrence Ephrim';
+export const SITE_DESCRIPTION = 'Welcome to my personal portfolio website, showcasing my projects, skills, and experience as a developer. Explore my work, blog posts, and more.';
+export const GENERATE_SLUG_FROM_TITLE = true
+export const TRANSITION_API = true
